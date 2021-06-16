361b343e1949ba861cccd3cadb8b56730b93a415
Luiz Alb. Silva <luizalbsilva@gmail.com>
2021-06-16 15:19:41 -0300

    React navigator

---
+++ components/home.screen.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {View, Text, StyleSheet} from "react-native";
+import {StatusBar} from "expo-status-bar";
+
+export default function HomeScreen() {
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
